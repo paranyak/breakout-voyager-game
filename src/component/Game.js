6cd1d13f515f6bfdc8cc1a7d7bf943d5773a8d45
paranyak <paranyak@ucu.edu.ua>
2018-03-19 13:56:13 +0200

    added svg with blocks.

--- src/component/Game.js
+++ src/component/Game.js
@@ -4,8 +4,8 @@ const height = 400;
 const width = 600;
 const animation = most.periodic(1000 / 60);
 
-const Ball = (ball, platform, x, y = 5) => {
-    console.log(platform, ball);
+const Ball = (ball, platform, x, y = 400) => {
+    console.log(x, y);
     ball.setAttribute("cx", x);
     ball.setAttribute("cy", y);
     let vy = 3;
@@ -16,7 +16,7 @@ const Ball = (ball, platform, x, y = 5) => {
                 vx = (nextX >= width || nextX < 0) ? -vx : vx;
                 return nextX + vx;
             },
-            0)
+            x)
         .observe((a) => {
             x = a;
             ball.setAttribute("cx", a);
@@ -24,14 +24,14 @@ const Ball = (ball, platform, x, y = 5) => {
         });
     const Ycoordinate = animation
         .scan((nextY) => {
-                if(nextY >= height && (platform.x.animVal.value > ball.cx.animVal.value || ball.cx.animVal.value > platform.x.animVal.value + platform.width.animVal.value)){
+                if(nextY >= height && (platform.x.animVal.value > ball.cx.animVal.value + 16 || ball.cx.animVal.value -16 > platform.x.animVal.value + platform.width.animVal.value)){
                     vx=0; vy =0;
                     return nextY +vy;
                 }
                 vy = (nextY >= height || nextY < 0) ? -vy : vy;
                 return nextY + vy;
             },
-            0).observe((a) => {
+            y).observe((a) => {
             ball.setAttribute("cy", a);
         });
 };
@@ -51,12 +51,33 @@ const Platform = (platform, x = 0) => {
     return platform;
 }
 
+const creatingBlocks = (rows, columns)=>{
+    console.log("creating svg");
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    let svgNS = svg.namespaceURI;
+    svg.setAttribute('class', 'Game__blocks');
+    let colors = ["#fad390", "#f6b93b", "#fa983a", "#e55039","#e17055",  "#e66767", "#b71540", "#c44569"];
+    for(let c =0; c < columns; c++) {
+        for (let i = 0; i < rows; i++) {
+            console.log("here");
+            let rect = document.createElementNS(svgNS, 'rect');
+            rect.setAttribute('x', (5 + 40 * i));   //rows
+            rect.setAttribute('y', 5 + 20*c);       //columns
+            rect.setAttribute('width', 30);
+            rect.setAttribute('height', 10);
+            rect.setAttribute('fill', colors[c]);
+            svg.appendChild(rect);
+            document.querySelector(".Game").appendChild(svg);
+        }
+    }
+}
+
 class Game {
     constructor() {
-        console.log("here");
-        let platform = Platform(document.querySelector('.Game__platform'), 5);
-        Ball(document.querySelector('.Game__ball'), platform, 0);
-
+        console.log("game constructor");
+        let platform = Platform(document.querySelector('.Game__platform'), 200);
+        //Ball(document.querySelector('.Game__ball'), platform, 200, 400);
+        creatingBlocks(15, 8);
     }
 }
 
